664d89e198c44abc5c0f76e34b9eb2b388afa61b
Spardotr <spardotr@unal.edu.co>
2025-01-14 21:34:41 -0500

    Update main.py

--- 5.2/main.py
+++ 5.2/main.py
@@ -1,1 +1,10 @@
+from shapes import Rectangle, Square, Scalene, Isosceles, TriRectangle
 
+if __name__ == "__main__":
+    scalene = Scalene(base=3, height=4, side1=3, side2=4, side3=5)
+    square = Square(side=4)
+    isosceles = Isosceles(base=3, height=4, side=5)
+
+    print(square.__str__())
+    print(scalene.__str__())
+    print(isosceles.__str__())
